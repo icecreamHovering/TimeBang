865faba1ca79782d8c27421cf1e825ae1a20a995
feiLong <pengfeilong20@163.com>
2019-07-08 01:06:00 +0800

    d

---
+++ client/loving/pages/me/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="index">
+		<image class="logo" src="/static/logo.png"></image>
+		<view>
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	@import './index';
+</style>
