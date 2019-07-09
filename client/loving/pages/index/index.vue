448567a61ddffeb3f833410ff59fc993c6c4b0d0
pengfeilong <pengfeilong20@163.com>
2019-07-09 20:07:12 +0800

    9

--- client/loving/pages/index/index.vue
+++ client/loving/pages/index/index.vue
@@ -3,11 +3,16 @@
 		<view class="index-swiper">
 			<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
 				<swiper-item>
-					<view class="swiper-item uni-bg-red">A</view>
+					<view class="swiper-item">A</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item">B</view>
 				</swiper-item>
 			</swiper>
 		</view>
-		<view></view>
+		<view class="index-tap">
+			<view class="index-tap-item" :class="{active: currentIndex === index}" v-for="(item,index) in tabArr" :key='index' @click="changeTab(index)">{{item.text}}</view>
+		</view>
 		<view></view>
 	</view>
 </template>
@@ -19,14 +24,29 @@
 				indicatorDots: true,
 				autoplay: true,
 				interval: 2000,
-				duration: 500
+				duration: 500,
+				currentIndex: 0,
+				tabArr: [
+					{
+						text: 'text1',
+						id: 0
+					},{
+						text: 'text2',
+						id: 1
+					},{
+						text: 'text3',
+						id: 2
+					}
+				]
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			changeTab(index) {
+				this.currentIndex = index;
+			}
 		}
 	}
 </script>
