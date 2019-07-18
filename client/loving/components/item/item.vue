facad8ff68ce30ac969f5abc80f1033df7640d97
feiLong <pengfeilong20@163.com>
2019-07-19 00:29:34 +0800

    0

---
+++ client/loving/components/item/item.vue
@@ -0,0 +1,30 @@
+<template>
+	<!--
+	* lotusNoData.isShow:设置显示无数据组件的显示隐藏
+	* lotusNoData.wrapStyle:设置最外部view的样式，垂直居中
+	* lotusNoData.imgStyle:设置图片大小，内外边距
+	* lotusNoData.imgUrl:设置图片链接
+	* lotusNoData.showTitleFlag:设置是否需要显示无数据提示语
+	* lotusNoData.titleStyle:设置无数据提示语的样式
+	* lotusNoData.title:设置无数据提示语的值
+	-->
+	<view class="component-item">
+		测试
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'items',
+		props:["showItem"],
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	@import "./item";
+</style>
