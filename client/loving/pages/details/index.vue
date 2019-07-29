cca26b8f0fc09766a654a2bad7d259c47068cb13
feiLong <pengfeilong20@163.com>
2019-07-30 00:29:12 +0800

    1

--- client/loving/pages/details/index.vue
+++ client/loving/pages/details/index.vue
@@ -1,6 +1,26 @@
 <template>
-	<view class="index">
-
+	<view class="details">
+		<view class="details-pictures">
+			<view class="details-pictures-images"></view>
+			<view class="details-pictures-info">
+				<view class="left">
+					<view>
+						<text>小姐姐</text>
+						<text>顶</text>
+						<image src="../../static/images/icon-phone.png"></image>
+					</view>
+					<view>
+						<text>20岁</text>
+						<text>藤县太平镇</text>
+					</view>
+				</view>
+				<view class="right">
+					<view>收藏</view>
+					<view>升级VIP</view>
+				</view>
+			</view>
+		</view>
+		<view class="details-material"></view>
 	</view>
 </template>
 
